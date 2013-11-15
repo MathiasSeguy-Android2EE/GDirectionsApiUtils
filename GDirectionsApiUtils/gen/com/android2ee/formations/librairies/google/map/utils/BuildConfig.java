242ca4385da82d1aa6d1daf771274ea197233bdf
MathiasSeguy-Android2EE <mathias.seguy.it@gmail.com>
2013-11-15 09:23:35 +0100

    The project has been updated for the version of GooglePlayService with the following number (nov 2013 release)
    <integer name="google_play_services_version">4030500</integer>
    
    I also clear some unused libs and add the bin\GDir.jar element in the
    repository.

---
+++ GDirectionsApiUtils/gen/com/android2ee/formations/librairies/google/map/utils/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.android2ee.formations.librairies.google.map.utils;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
